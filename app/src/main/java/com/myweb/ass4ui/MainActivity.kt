9f7b25ba704153e7d03677a87e5ef438a7a4fad1
seth-setthaphong <70099308+sethhyyy@users.noreply.github.com>
2020-08-30 23:07:23 +0700

    ส่งงาน Ass4 Sec3

---
+++ app/src/main/java/com/myweb/ass4ui/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.myweb.ass4ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import com.myweb.ass4ui.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun showInfo(view:View){
+        val radio: RadioButton = findViewById(radioGroup.checkedRadioButtonId)
+        text_show.text = "Name: ${edit_username.text}\nPassword: ${edit_password.text}\nGender: ${radio.text}\nE-mail: ${edit_mail.text}\nBirthday: ${text_date.text}"
+    }
+    fun showDatePickerDialog(v: View){
+        val newDateFragment = DatePickerFragment()
+        newDateFragment.show(supportFragmentManager, "Date Picker")
+    }
+    fun radio_button_click(view: View){
+        // Get the clicked radio button instance
+        val radio: RadioButton = findViewById(radioGroup.checkedRadioButtonId)
+    }
+    fun reset(v:View){
+        edit_username.text.clear()
+        edit_password.text.clear()
+        male.setChecked(false)
+        female.setChecked(false)
+        edit_mail.text.clear()
+        text_date.text = "mm / dd / yy"
+        text_show.text = "Show Information"
+    }
+
+}
